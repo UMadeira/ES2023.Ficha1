3804b120fbed7344c516c80e94bede400f784a7c
Leonel Nobrega <lnobrega@uma.pt>
2023-03-02 14:26:57 +0000

    Initial

---
+++ Ex01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Engenharia de Software - Ficha Prática 1 - Exercício 1");
+
+Console.Write("Introduzir número inteiro: ");
+var x = Console.ReadLine();
